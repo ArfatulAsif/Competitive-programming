ff12d06c470f91379025aa7a024150ec71c6705e
Arfatul Islam Asif <107148686+ArfatulAsif@users.noreply.github.com>
2023-05-29 02:50:31 +0600

    Add files via upload

---
+++ AAAAAAAA.cpp
@@ -0,0 +1,143 @@
+=============================== LCA AND BINARY LIFTING ================
+=======================================================================
+
+
+
+
+//Intermediary
+//Young kid on the block
+//AIAsif try's "Continuing the journey"
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define endl "\n"
+#define int long long int
+#define ordered_set tree< int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+const int N = 1e5+100;
+vector<int>graph[N];
+bool vis[N];
+int din[N];
+int dout[N];
+int tin = 0;
+int tout = 0;
+const int L = log2(N);
+int up[N][L+1];
+
+void dfs(int s,int par)
+{
+        vis[s] = true;
+        din[s] = tin++;
+
+        up[s][0] = par;
+
+        for(int i=1;i<=L;i++)
+        {
+                up[s][i] = up[ up[s][i-1] ][i-1]; // Here is the binary lifting is occuring try for this graph ( 1->2 , 2->3 , 3->4 , 4->5,5->6,6->7,7->8,8->9,9->10) then s = 10 er jonno eta draw kore buja
+        }
+
+
+        for(auto child : graph[s])
+        {
+                if(vis[child])
+                {
+                        continue;
+                }
+                dfs(child,s);
+        }
+        dout[s] = tin++;
+}
+
+bool is_ancestor(int u,int v) // u is ancestor of v
+{
+        if(din[u]<=din[v] && dout[u]>=dout[v])
+        {
+                return true;
+        }
+        return false;
+}
+
+
+
+int lca(int u,int v)
+{
+        if(is_ancestor(u,v))
+        {
+                return u;
+        }
+        if(is_ancestor(v,u))
+        {
+                return v;
+        }
+        for(int i=L;i>=0;i--)
+        {
+                if(!is_ancestor(up[u][i] ,v))  // ancestor na hole jump dissi                                [up[1][0] = 1 na hole ei part kaj korbe na]
+                {
+                        u = up[u][i];
+                }
+        }
+        return up[u][0];
+
+}
+
+
+int32_t main()
+{
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        int n,m;
+        cin>>n>>m;
+
+        for(int i=0;i<m;i++)
+        {
+                int x,y;
+                cin>>x>>y;
+                graph[x].push_back(y);
+                graph[y].push_back(x);
+        }
+
+        dfs(1,1); // parent of 1 should be 1 i.e. up[1][0] = 1;
+
+        int q;
+        cin>>q;
+        while(q--)
+        {
+                int u,v;
+                cin>>u>>v;
+                cout<<lca(u,v)<<endl;
+        }
+
+
+
+
+}
+
+
+
+
+/*
+7 6
+1 2
+1 3
+2 4
+2 5
+3 6
+3 7
+5
+4 5
+5 6
+7 2
+6 7
+6 3
+
+
+o/p
+2
+1
+1
+3
+3
+
+*/
