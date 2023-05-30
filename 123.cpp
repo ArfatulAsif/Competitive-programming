bad7701ae2adef23e47cced5de3935c5ab89f677
Arfatul Islam Asif <107148686+ArfatulAsif@users.noreply.github.com>
2023-05-31 03:19:07 +0600

    Add files via upload

---
+++ 123.cpp
@@ -0,0 +1,90 @@
+//Intermediary
+//Young kid on the block
+//AIAsif try's "Continuing the journey"
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define endl "\n"
+#define int long long int
+#define ordered_set tree< int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+//TOPO SORT
+
+
+const int N = 1e5+100;
+vector<int>graph[N];
+bool vis[N];
+int in[N];
+
+int32_t main()
+{
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+
+        int n,m;
+        cin>>n>>m;
+
+        for(int i=0;i<m;i++)
+        {
+                int x,y;
+                cin>>x>>y;
+                graph[x].push_back(y);
+                in[y]++;
+        }
+        queue<int>q;
+        for(int i=1;i<=n;i++)
+        {
+                if(in[i]==0)
+                {
+                    q.push(i);
+                }
+        }
+        vector<int>ans;
+        while(!q.empty())
+        {
+                int father = q.front();
+                vis[father] = true;
+                ans.push_back(father);
+                q.pop();
+                for(auto child : graph[father])
+                {
+                        if(vis[child])
+                        {
+                                continue;
+                        }
+                        in[child]--;
+                        if(in[child]==0)
+                        {
+                                q.push(child);
+                                vis[child] = true;
+                        }
+                }
+        }
+
+
+        for(int i=1;i<=n;i++)
+        {
+                if(vis[i]==false)
+                {
+                        cout<<"IMPOSSIBLE"<<endl;
+                        return 0;
+                }
+        }
+
+
+        for(auto x : ans)
+        {
+                cout<<x<<" ";
+        }
+        cout<<endl;
+
+        return 0;
+
+
+
+
+
+
+}
