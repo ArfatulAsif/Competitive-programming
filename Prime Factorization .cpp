57602a844070471945ae22d03e1fb6b0c5b57bba
Arfatul Islam Asif <107148686+ArfatulAsif@users.noreply.github.com>
2023-05-31 03:34:23 +0600

    Update and rename 123.cpp to Prime Factorization .cpp

--- Prime Factorization .cpp
+++ Prime Factorization .cpp
@@ -1,14 +1,5 @@
-//Intermediary
-//Young kid on the block
-//AIAsif try's "Continuing the journey"
-#include<bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace std;
-using namespace __gnu_pbds;
-#define endl "\n"
-#define int long long int
-#define ordered_set tree< int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+//Just copy this and move on to the solution 
 
 const int N = 1e6;
 bool sieve[N];
