c91cc99020266a36af8546f2a77e10d18e961272
Arfatul Islam Asif <107148686+ArfatulAsif@users.noreply.github.com>
2023-05-31 03:25:54 +0600

    Update and rename 123.cpp to Negative Cycle Printing .cpp

--- Negative Cycle Printing .cpp
+++ Negative Cycle Printing .cpp
@@ -1,14 +1,4 @@
-//Intermediary
-//Young kid on the block
-//AIAsif try's "Continuing the journey"
-#include<bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace std;
-using namespace __gnu_pbds;
-#define endl "\n"
-#define int long long int
-#define ordered_set tree< int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+//AIA
 
 //using floyed printing neg cycle
 
