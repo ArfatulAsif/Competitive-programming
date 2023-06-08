42d6e24c53272e96f5295f9788802591a753854c
Arfatul Islam Asif <107148686+ArfatulAsif@users.noreply.github.com>
2023-06-09 01:15:22 +0600

    Add files via upload

---
+++ trie.cpp
@@ -0,0 +1,149 @@
+//Intermediary
+//Young kid on the block
+//AIAsif try's "Continuing the journey"
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define endl "\n"
+#define int long long int
+#define ordered_set tree< int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+const int N = 1e6;
+int node[N][26];
+int root = 0;
+int avail = 0;
+int cnt[N];
+int is_end[N];
+
+void ini()
+{
+        root = 0;
+        avail = 0;
+        for(int i=0;i<26;i++)
+        {
+                node[root][i] = -1;
+        }
+}
+
+
+void Insert(string s)
+{
+
+        int cur = root;
+        for(int i=0;i<s.size();i++)
+        {
+                if(node[cur][s[i]-'a']==-1)
+                {
+                        avail++;
+
+                        node[cur][s[i]-'a'] = avail;
+
+                        is_end[avail] = 0;
+
+                        cnt[avail] = 0;
+
+                        for(int j=0;j<26;j++)
+                        {
+                                node[avail][j] = -1;
+                        }
+                }
+
+                cur = node[cur][s[i]-'a'];
+
+                cnt[cur]++;
+        }
+        is_end[cur]++;
+}
+
+void Remove(string s)
+{
+
+        int cur = root;
+
+        for(int i=0;i<s.size();i++)
+        {
+
+                cur = node[cur][s[i]-'a'];
+
+                cnt[cur]--;
+        }
+        is_end[cur]--;
+}
+
+bool prefix(string s)
+{
+
+        int cur = root;
+        for(int i=0;i<s.size();i++)
+        {
+                if(node[cur][s[i]-'a']!=-1 && cnt[node[cur][s[i]-'a'] ]> 0)
+                {
+                        cur = node[cur][s[i]-'a'];
+                }
+                else
+                {
+                        return false;
+                }
+        }
+
+        return true;
+}
+
+
+bool Search(string s)
+{
+        int cur = root;
+        for(int i=0;i<s.size();i++)
+        {
+                if(node[cur][s[i]-'a']!=-1 && cnt[node[cur][s[i]-'a'] ]> 0)
+                {
+                        cur = node[cur][s[i]-'a'];
+                }
+                else
+                {
+                        return false;
+                }
+        }
+
+        return (is_end[cur]>0);
+}
+
+
+
+int32_t main()
+{
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+
+
+        ini();
+
+        int n;
+        cin>>n;
+
+        while(n--)
+        {
+                string s;
+                cin>>s;
+                Insert(s);
+        }
+
+        string a;
+        cin>>a;
+
+        cout<<prefix(a)<<endl;
+        cout<<Search(a)<<endl;
+
+        string b;
+
+        cin>>b;
+
+        Remove(b);               // asuming b is present in trie
+
+        cout<<Search(a)<<endl;
+
+        return 0;
+
+}
