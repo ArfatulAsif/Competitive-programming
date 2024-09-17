af0fa5055c87266f138cccc9083a2043b9f2079e
Arfatul Islam Asif <107148686+ArfatulAsif@users.noreply.github.com>
2024-09-17 08:07:09 +0600

    Update Centroid Decomposition .cpp

--- Centroid Decomposition .cpp
+++ Centroid Decomposition .cpp
@@ -2,187 +2,278 @@
 
 ================================================================        =======================================
 
-
 //How many paths length = k   
 
+const int N = 2e5 + 100;
 
-
-const int N = 5e4+10;
-
-class CentroidDecomposition
-{
+class CentroidDecomposition {
 private:
-        int n,k;
-        set<int>graph[N];  // many implementation uses 2d vector and isRemoved array , I however think using set is way simplier
-
-        int cnt[N];
-
-        int subtree[N];
-
-        int mx_depth;
+    int n, k;
+    set<int> graph[N];
+    int cnt[N];    // cnt[i] = number of i-length path from the centroid/root
+    int subtree[N];
+    int mx_depth;
+    int parent[N];
 
 public:
+    long long int ans; // ans = total count of exactly k-length paths
 
+    CentroidDecomposition() {
+        this->n = 0;
+        this->k = 0;
+    }
 
-        int ans;
+    void addEdge(int u, int v) {
+        graph[u].insert(v);
+        graph[v].insert(u);
+    }
 
+    int dfs(int s, int p) {
+        subtree[s] = 1;
+        for (auto child: graph[s]) {
+            if (child == p) {
+                continue;
+            }
+            subtree[s] += dfs(child, s);
+        }
+        return subtree[s];
+    }
 
+    int centroid(int s, int p, int n) {
+        for (auto child: graph[s]) {
+            if (child == p) {
+                continue;
+            }
+            if (subtree[child] > n / 2) {
+                return centroid(child, s, n);
+            }
+        }
+        return s;
+    }
 
-        CentroidDecomposition(int n,int k)
-        {
-                this->n = n;
-                this->k = k;
-                ans = 0;
+    void build(int s, int p, int k) {
+        this->k = k;
+        int n = dfs(s, p);
+        int c = centroid(s, p, n);
 
+        vector<int> temp(graph[c].begin(), graph[c].end());
+
+        do_for_centroid(c);
+
+        for (auto child: temp) {
+            graph[c].erase(child); // tearing apart the graph
+            graph[child].erase(c);
+            build(child, c, k);
+        }
+    }
+
+    void do_for_centroid(int c) { // Here, we will calculate how many paths that contain the centroid C
+        cnt[0] = 1; // for computation, we consider Centroid to Centroid, zero length path
+
+        mx_depth = 0;
+
+        for (auto child: graph[c]) {
+            // current depth = 1, since they are children
+            dfs2(child, c, 1, true);  // flag = true, means compute k-length path for this subtree.
+            dfs2(child, c, 1, false); // flag = false means calculate cnt[i], for each i-length path from the centroid to subtree node.
+
+            // For each child, we get a different subtree,
+            // We know that if we go from one subtree node to another subtree node, we must visit the centroid.
+            // Since we are calculating the k-length path that goes through the centroid, we must process each subtree differently.
+            // Also, to avoid recomputation, we must use two different dfs2() calls, so that for each i, we don't count cnt[k-i] of the same subtree, which wouldn't visit through the centroid.
         }
 
-        void addEdge(int u,int v)
-        {
-                graph[u].insert(v);
-                graph[v].insert(u);
+        for (int i = 0; i <= mx_depth + 1; i++) {
+            cnt[i] = 0; // clearing
+        }
+    }
+
+    void dfs2(int s, int p, int depth, bool flag) {
+        if (k - depth < 0) {
+            return;
         }
 
-        int dfs(int s,int p)
-        {
-                subtree[s] = 1;
-                for(auto child : graph[s])
-                {
-                        if(child == p )
-                        {
-                                continue;
-                        }
-                        subtree[s] += dfs(child,s);
-                }
-                return subtree[s];
+        if (flag) { // calculate k-length path
+            ans += cnt[k - depth];  // from all the visited different subtrees, we get the count of k-depth path,
+                                    // from this subtree we take the current path, whose length is depth, so total length of each path = k - depth + depth = k
+        } else { // flag = false
+            cnt[depth]++;
         }
 
-        int centroid(int s,int p,int n)
-        {
-                for(auto child : graph[s])
-                {
-                        if(child==p)
-                        {
-                                continue;
-                        }
-                        if(subtree[child]>n/2)
-                        {
-                                return centroid(child,s,n);
-                        }
-                }
-                return s;
+        mx_depth = max(depth, mx_depth);
+
+        for (auto child: graph[s]) {
+            if (child == p) {
+                continue;
+            }
+
+            dfs2(child, s, depth + 1, flag);
         }
+    }
 
-        void build(int s,int p)
-        {
-                int n = dfs(s,p);
-                int c = centroid(s,p,n);
+} CD;
 
-                vector<int>temp(graph[c].begin() , graph[c].end());
+int32_t main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-                do_for_centroid(c);
+    int n, k;
+    cin >> n >> k;
 
-                for(auto child : temp)
-                {
-                        graph[child].erase(c);
-                        graph[c].erase(child);
+    for (int i = 0; i < n - 1; i++) {
+        int a, b;
+        cin >> a >> b;
+        CD.addEdge(a, b);
+    }
 
-                        build(child , c);
-                }
+    CD.build(1, 1, k);
 
-
-        }
-
-        void do_for_centroid(int c)
-        {
-                mx_depth = 0;
-
-                cnt[0] = 1;
-
-                for(auto child : graph[c])
-                {
-                        dfs2(child,c,1 , false); // we store ans  in this step :
-                        dfs2(child,c,1,true);  // in order to avoid recalculation , we store cnt[] after ans has been store for this path
-                }
-
-
-
-
-                for(int i=0;i<=mx_depth;i++) // (logn)
-                {
-                        cnt[i] = 0;
-                }
-
-        }
-
-
-        void dfs2(int s,int p , int depth , bool storeValue)
-        {
-                if(k-depth<0)
-                {
-                        return;
-                }
-
-                mx_depth = max(mx_depth , depth);
-
-                if(storeValue==false)
-                {
-                        ans += cnt[k-depth];
-                }
-                else if(storeValue)
-                {
-                        cnt[depth]++;
-                }
-
-                for(auto child : graph[s])
-                {
-                        if(child == p)
-                        {
-                                continue;
-                        }
-                        dfs2(child,s,depth+1,storeValue);
-                }
-        }
-
-
-};
-
-
-int32_t main()
-{
-        ios::sync_with_stdio(0);
-        cin.tie(0);
-
-        int n,k;
-        cin>>n>>k;
-
-        CentroidDecomposition cd(n,k);
-
-        for(int i=0;i<n-1;i++)
-        {
-                int u,v;
-                cin>>u>>v;
-
-                cd.addEdge(u,v);
-        }
-
-
-        cd.build(1,0);
-
-
-        cout<<cd.ans<<endl;
-
-
-        return 0;
-
-
-
-
-
+    cout << CD.ans << endl;
 }
 
+------------------ Faster -------------------
+
+
+        
+// use processed[i] to mark which node is already removed, like this implementation        
+        
+const int N = 2e5 + 100;
+
+class CentroidDecomposition {
+private:
+    int n, k;
+    vector<int> graph[N];
+    bool processed[N]; // processed[i] = i-th node has been removed from the graph
+    int cnt[N];    // cnt[i] = number of i-length paths from the centroid/root
+    int subtree[N];
+    int mx_depth;
+    int parent[N];
+
+public:
+    long long int ans; // ans = total count of exactly k-length paths
+
+    CentroidDecomposition() {
+        this->n = 0;
+        this->k = 0;
+        fill(processed, processed + N, false);
+    }
+
+    void addEdge(int u, int v) {
+        graph[u].push_back(v);
+        graph[v].push_back(u);
+    }
+
+    int dfs(int s, int p) {
+        subtree[s] = 1;
+        for (auto child : graph[s]) {
+            if (child == p || processed[child]) {
+                continue;
+            }
+            subtree[s] += dfs(child, s);
+        }
+        return subtree[s];
+    }
+
+    int centroid(int s, int p, int n) {
+        for (auto child : graph[s]) {
+            if (child == p || processed[child]) {
+                continue;
+            }
+            if (subtree[child] > n / 2) {
+                return centroid(child, s, n);
+            }
+        }
+        return s;
+    }
+
+    void build(int s, int p, int k) {
+        this->k = k;
+        int n = dfs(s, p);
+        int c = centroid(s, p, n);
+
+        processed[c] = true;
+
+        do_for_centroid(c);
+
+        for (auto child : graph[c]) {
+            if (processed[child]) {
+                continue;
+            }
+            build(child, c, k);
+        }
+    }
+
+    void do_for_centroid(int c) { // Calculate how many paths contain the centroid C
+        cnt[0] = 1; // for computation, we consider Centroid to Centroid, zero length path
+
+        mx_depth = 0;
+
+        for (auto child : graph[c]) {
+            if (processed[child]) {
+                continue;
+            }
+
+            // Current depth = 1, since they are children
+            dfs2(child, c, 1, true);  // flag = true, compute k-length path for this subtree.
+            dfs2(child, c, 1, false); // flag = false, calculate cnt[i], for each i-length path from centroid to subtree node.
+
+            // For each child, we get a different subtree.
+            // We know that if we go from one subtree node to another subtree node, we must visit the centroid.
+            // Since we are calculating the k-length path that goes through the centroid, we must process each subtree differently.
+            // To avoid recomputation, we use two different dfs2() calls.
+        }
+
+        for (int i = 0; i <= mx_depth + 1; i++) {
+            cnt[i] = 0; // Clearing
+        }
+    }
+
+    void dfs2(int s, int p, int depth, bool flag) {
+        if (k - depth < 0) {
+            return;
+        }
+
+        if (flag) { // Calculate k-length path
+            ans += cnt[k - depth];  // From all the visited different subtrees, we get the count of k-depth path,
+                                    // from this subtree we take the current path, whose length is depth, so total length of each path = k - depth + depth = k
+        } else { // flag = false
+            cnt[depth]++;
+        }
+
+        mx_depth = max(depth, mx_depth);
+
+        for (auto child : graph[s]) {
+            if (child == p || processed[child]) {
+                continue;
+            }
+
+            dfs2(child, s, depth + 1, flag);
+        }
+    }
+} CD;
+
+int32_t main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n, k;
+    cin >> n >> k;
+
+    for (int i = 0; i < n - 1; i++) {
+        int a, b;
+        cin >> a >> b;
+        CD.addEdge(a, b);
+    }
+
+    CD.build(1, 1, k);
+
+    cout << CD.ans << endl;
+}
+
+        
 
 =================================================================  ==========================================
-//Closest Red vertex 
+//Closest Red vertex : https://codeforces.com/contest/342/problem/E
 
 const int inf = 1e9;
 
